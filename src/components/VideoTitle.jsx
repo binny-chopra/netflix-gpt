6e7b9783057d16346ddff1bdc00df56e637b14bc
Binny Chopra <159.binny@gmail.com>
2025-04-10 19:46:30 -0400

    main container video bg and title

---
+++ src/components/VideoTitle.jsx
@@ -0,0 +1,18 @@
+const VideoTitle = ({ title, overview }) => {
+  return (
+    <div className="w-screen aspect-video pt-[20%] px-24 text-white absolute bg-gradient-to-r from-black">
+      <h1 className="text-6xl font-bold"> {title}</h1>
+      <p className="py-6 text-lg w-1/2">{overview}</p>
+      <div>
+        <button className="cursor-pointer bg-white text-black text-xl p-4 w-40 rounded-lg hover:bg-white/80">
+          Play
+        </button>
+        <button className="mx-2 cursor-pointer bg-gray-500/50 text-white text-xl p-4 w-40 rounded-lg">
+          More Info
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default VideoTitle;
