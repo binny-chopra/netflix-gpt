09060ca70d78ad0db24231c93ce8362ea918987b
Binny Chopra <159.binny@gmail.com>
2025-04-10 22:17:28 -0400

    Filled Secondary Container with Movies List

--- src/components/VideoTitle.jsx
+++ src/components/VideoTitle.jsx
@@ -1,8 +1,8 @@
 const VideoTitle = ({ title, overview }) => {
   return (
-    <div className="w-screen aspect-video pt-[20%] px-24 text-white absolute bg-gradient-to-r from-black">
+    <div className="w-screen aspect-video pt-[10%] px-24 text-white absolute bg-gradient-to-r from-black">
       <h1 className="text-6xl font-bold"> {title}</h1>
-      <p className="py-6 text-lg w-1/2">{overview}</p>
+      <p className="py-6 text-lg w-3/4">{overview}</p>
       <div>
         <button className="cursor-pointer bg-white text-black text-xl p-4 w-40 rounded-lg hover:bg-white/80">
           Play
